8719b52844989470426f1a525085497eb13cbdb4
zmatsudo@hawaii.edu <112070149+zmatsudo@users.noreply.github.com>
2023-04-26 21:41:55 -1000

    Initial commit

---
+++ set_price.js
@@ -0,0 +1,11 @@
+function set_price(item_id, products) {
+    if (item_id === "*") {
+        for (let i=0; i < products.length; i++) {
+            set_new_price(products[i].price);
+        }
+    } 
+    };
+
+function set_new_price(products, dynamic) {
+    if (dynamic = true )
+};
